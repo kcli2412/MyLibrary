8fdf39a3788906056cbedae49c51e41c522b0895
kcli2412 <kcli2412@gmail.com>
2018-01-15 16:22:22 +0800

    Menu

--- app/src/main/java/com/example/student/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/student/mylibrary/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.student.mylibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.menu_add:
+                break;
+            case R.id.menu_options:
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
